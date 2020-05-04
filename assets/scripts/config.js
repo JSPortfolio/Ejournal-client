d90f5570da8d2650e101e45eec091dafd443900b
JSpringwater92 <jeffreyspringwater@gmail.com>
2020-05-04 14:11:48 -0400

    //Fixed Production link for API

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://blooming-caverns-70567.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
